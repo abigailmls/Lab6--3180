d945e0f4df0d305c5e7ec29632eaf45887efc4d5
abigailmls <abimiles25@gmail.com>
2020-05-20 16:38:46 -0500

    Lab 6

---
+++ info3180-lab6/app/static/js/app.js
@@ -0,0 +1,192 @@
+/* Add your Application JavaScript */
+Vue.component('app-header', {
+    template: `
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+              <a class="navbar-brand" href="#">VueJS App</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <router-link to="/" class="nav-link">Home</router-link>
+                    // <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+        </header>    
+    `,
+    data: function() {
+    return {};
+    }
+    });
+
+Vue.component('app-footer', {
+template: `
+<footer>
+  <div class="container">
+      <p>Copyright &copy {{ year }} Flask Inc.</p>
+  </div>
+</footer>
+`,
+    data: function() {
+        return {
+            year: (new Date).getFullYear()
+        }
+    }
+});
+
+Vue.component('app-header', {
+  template: `
+      <header>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+            <a class="navbar-brand" href="#">VueJS App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <router-link to="/" class="nav-link">Home</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/news" class="nav-link">News</router-link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+      </header>    
+  `,
+  data: function() {}
+});
+
+Vue.component('app-footer', {
+  template: `
+      <footer>
+          <div class="container">
+              <p>Copyright &copy {{ year }} Flask Inc.</p>
+          </div>
+      </footer>
+  `,
+  data: function() {
+      return {
+          year: (new Date).getFullYear()
+      }
+  }
+});
+
+const NewsList= Vue.component('news-list', {
+  template: `
+     <div class="news">
+          <h2>News</h2>
+            <ul class="news__list">
+              <li class="news__item">News item 1</li>
+              <li class="news__item">News item 2</li>
+              <li class="news__item">News item 3</li>
+           </ul>
+          <div class="form-inline d-flex justify-content-center">
+              <div class="form-group mx-sm-3 mb-2">
+                  <label class="sr-only" for="search">Search</label>
+                  <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+                  <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+              </div>
+          </div>
+          <ul class="news__list">
+            <li v-for="article in articles" 
+    class="news__item">{{ article.title }}</li>
+          </ul> 
+      </div>
+  `,
+  created: function() {
+      fetch('https://newsapi.org/v2/top-headlines? country=us&apiKey=<>')
+      .then(function(response) {
+          return response.json();
+          
+      })
+      .then(function(data) {
+      console.log(data);
+      self.articles = data.articles;
+      });
+  },
+  data: function() {
+      return {
+          articles: []
+          
+      }
+  },
+  data: function() {
+      return {
+          articles: [],
+          searchTerm: ''
+          
+      }
+      
+  },
+  methods: {
+      searchNews: function() {
+          let self = this;
+          fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en&apiKey=<>')
+          .then(function(response) {
+              return response.json();
+              
+          })
+          .then(function(data) {
+              console.log(data);
+              self.articles = data.articles;
+              
+          });
+          
+      }
+      
+  }
+  
+});
+
+const Home = Vue.component('home', {
+template: `
+<div class="home">
+<img src="/static/images/logo.png" alt="VueJS Logo">
+<h1>{{ welcome }}</h1>
+</div>
+`,
+data: function() {
+return {
+welcome: 'Hello World! Welcome to VueJS'
+}
+}
+});
+
+const router = new VueRouter({
+mode: 'history',
+routes: [
+{ path: '/', component: Home },
+{ path: '/news', component: NewsList }
+]
+});
+
+
+const app = new Vue({
+el: '#app',
+router
+});
+
+
+
+
+
+/* let app = new Vue({
+    el: '#app',
+    data: {
+        welcome: 'Hello World! Welcome to VueJS'
+    }
+}); */
+
+
+
